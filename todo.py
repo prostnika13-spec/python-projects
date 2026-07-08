15991f857e5b4f9f4fa1a09ff65f5a911d8f7cd7
Nika Lapo4ka <prost.nika13@gmail.com>
2026-07-08 10:35:50 +0300

    upd 2
    теперь терминал на русском :)

--- todo.py
+++ todo.py
@@ -1,7 +1,48 @@
 import click
 import os
+import sys
 
-@click.group()
+# Устанавливаем UTF-8 для вывода в консоль Windows
+sys.stdout.reconfigure(encoding='utf-8')
+
+class RussianHelpGroup(click.Group):
+    def format_help(self, ctx: click.Context, formatter):
+        # Заголовок "Использование:"
+        formatter.write_heading("Использование:")
+        formatter.write_text(f"  {self.get_usage(ctx)}")
+        formatter.write_text("")
+
+        # Опции 
+        formatter.write_heading("Опции:")
+        formatter.write_text("  --help  Показать это сообщение и выйти.")
+        formatter.write_text("")
+
+        # Команды
+        if self.commands:
+            formatter.write_heading("Команды:")
+            self._format_commands(ctx, formatter)
+            formatter.write_text("")
+
+    def _format_commands(self, ctx, formatter):
+        """Форматирует список команд с русскими описаниями"""
+        commands = []
+        for name, command in self.commands.items():
+            if name == "help":
+                continue
+            help_text = command.help or ""
+            commands.append((name, help_text))
+        if commands:
+            commands.sort()
+            for name, help_text in commands:
+                formatter.write_text(f"  {name:<10} {help_text}")
+
+    # Отключаем автоматическое добавление стандартной опции --help
+    def get_help_options(self, ctx):
+        return []
+
+
+# Основная группа команд с русской справкой
+@click.group(cls=RussianHelpGroup)
 @click.pass_context
 def todo(ctx):
     '''Простое CLI-приложение для списка задач'''
@@ -35,6 +76,7 @@ def todo(ctx):
             task_id, task_text = line.split('|||', 1)
             ctx.obj['TASKS'][task_id] = task_text
 
+
 @todo.command()
 @click.pass_context
 def tasks(ctx):
@@ -47,6 +89,7 @@ def tasks(ctx):
     else:
         click.echo('Задач пока нет. Используйте ADD для добавления.\n')
 
+
 @todo.command()
 @click.pass_context
 @click.option('-add', '--add_task', prompt='Введите задачу для добавления')
@@ -54,12 +97,12 @@ def add(ctx, add_task):
     '''Добавить новую задачу'''
     add_task = add_task.strip()
     if not add_task:
-        click.echo('❌ Задача не может быть пустой.')
+        click.echo(' Задача не может быть пустой.')
         return
 
     new_id = str(ctx.obj['LATEST'])
     ctx.obj['TASKS'][new_id] = add_task
-    click.echo(f'✅ Добавлена задача "{add_task}" с ID {new_id}')
+    click.echo(f' Добавлена задача "{add_task}" с ID {new_id}')
 
     new_latest = ctx.obj['LATEST'] + 1
     with open('./todo.txt', 'w') as f:
@@ -67,6 +110,7 @@ def add(ctx, add_task):
         for tid, ttext in ctx.obj['TASKS'].items():
             f.write(f'{tid}|||{ttext}\n')
     ctx.obj['LATEST'] = new_latest
+
 
 @todo.command()
 @click.pass_context
@@ -79,7 +123,7 @@ def done(ctx, fin_taskid):
         return
 
     task_text = ctx.obj['TASKS'].pop(key)
-    click.echo(f'✅ Завершена и удалена задача "{task_text}" с ID {fin_taskid}')
+    click.echo(f' Завершена и удалена задача "{task_text}" с ID {fin_taskid}')
 
     if ctx.obj['TASKS']:
         with open('./todo.txt', 'w') as f:
@@ -90,6 +134,7 @@ def done(ctx, fin_taskid):
         ctx.obj['LATEST'] = 0
         with open('./todo.txt', 'w') as f:
             f.write('0\n')
+
 
 if __name__ == '__main__':
     todo()
